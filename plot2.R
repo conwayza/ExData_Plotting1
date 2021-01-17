b7f6c8879875287232ec33849d35f146cebfac1d
conwayza <77240614+conwayza@users.noreply.github.com>
2021-01-17 13:54:55 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,1 +1,6 @@
+#getting data
+dataFile <- "./household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+
 
