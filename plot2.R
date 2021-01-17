8ec6d1d5af74cdaa3e25c3dc4b22c18d4f30f525
conwayza <77240614+conwayza@users.noreply.github.com>
2021-01-17 13:59:39 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,4 +3,14 @@ dataFile <- "./household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 
+#subsetting data
+data <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02")) 
 
+#Plot 2
+dateTime <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+globalActivePower <- as.numeric(data$Global_active_power)
+plot(dateTime, globalActivePower,  type="l", main="Global Active Power", xlab="", ylab="Global Active Power")
+
+#Saving as png
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
