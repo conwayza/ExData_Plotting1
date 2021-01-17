7b66b0a75e1405daf9728d33332b48cd80005b12
conwayza <77240614+conwayza@users.noreply.github.com>
2021-01-17 12:50:56 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,7 +1,11 @@
+#getting data
 dataFile <- "./household_power_consumption.txt"
-completeData <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subsetData <- completeData[completeData$Date %in% c("1/2/2007", "2/2/2007) ,]
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 
+#subsetting
+subsetData <- data[data$Date %in% c("1/2/2007", "2/2/2007) ,]
+
+#plotting histogram plot1
 globalActivePower <- as.numeric(subsetData$Global_active_power)
 png("plot1.png", width=480, height=480")
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (in kW)")
