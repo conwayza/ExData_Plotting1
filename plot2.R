3bdda1350153379c9938ed35fd97e86a68320337
conwayza <77240614+conwayza@users.noreply.github.com>
2021-01-17 14:17:19 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -9,7 +9,7 @@ data <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 #Plot 2
 dateTime <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 globalActivePower <- as.numeric(data$Global_active_power)
-plot(dateTime, globalActivePower,  type="l", main="Global Active Power", xlab="", ylab="Global Active Power")
+plot(globalActivePower ~ dateTime,  type="", main="Global Active Power", xlab="", ylab="Global Active Power")
 
 #Saving as png
 dev.copy(png, file="plot2.png", height=480, width=480)
