975ab0d16b7ed7612723ea59fcef1906f637658b
conwayza <77240614+conwayza@users.noreply.github.com>
2021-01-17 15:00:02 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -5,7 +5,7 @@ data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec="
 #subsetting data
 subsetData <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))  
 
-dateTime <- strptime(paste(subsetData$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+dateTime <- strptime(paste(subsetData$Date, subsetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 globalActivePower <- as.numeric(subsetData$Global_active_power)
 subMetering1 <- as.numeric(subsetData$Sub_metering_1)
 subMetering2 <- as.numeric(subsetData$Sub_metering_2)
