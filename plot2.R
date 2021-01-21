57f4b3452184a0e1882b11593bdd074a0d7ee6cb
conwayza <77240614+conwayza@users.noreply.github.com>
2021-01-21 10:16:18 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,16 +1,19 @@
 #getting data
 dataFile <- "./household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+data <- read.table(dataFile, sep=";", skip=1, dec=".")
+names(data) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 
 #subsetting data
-data <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02")) 
+subdata <- subset(data, subset=(data$Date=="1/2/2007" | data$Date=="2/2/2007")) 
+subdata$Date <- as.Date(subdata$Date, format="%d/%m/%Y")
+subdata$Time <- strptime(subdata$Time, format="%H:%M:S%")
+subdata[1:1440, "Time"] <- format(subdata[1:1440, "Time"], "2007-02-01 %H:%M:S%")
+subdata[1441:2880, "Time"] <- format(subdata[1441:2880, "Time"], "2007-02-01 %H:%M:S%")
 
-#Plot 2
-dateTime <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
-globalActivePower <- as.numeric(data$Global_active_power)
-plot(globalActivePower, dateTime,  type="l", main="Global Active Power", xlab="", ylab="Global Active Power")
-
-#Saving as png
+#Plotting and saving the data 
 dev.copy(png, file="plot2.png", height=480, width=480)
+plot(subdata$Time, as.numeric(as.character(subdata$Global_active_power)), type="l", xlab="", ylab="Global Active Power (In KW)")
 dev.off()
+
+
+
