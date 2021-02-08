5f8431970bbffa6c1ff7d82f17703346495a3bd9
conwayza <77240614+conwayza@users.noreply.github.com>
2021-02-08 08:20:26 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,19 +1,11 @@
-#getting data
+##Collecting the information from the text file and creating a subset for it
 dataFile <- "./household_power_consumption.txt"
-data <- read.table(dataFile, sep=";", skip=1, dec=".")
-names(data) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subsetDataFile <- data[data$Date %in% c("1/2/2007", "2/2/2007") ,]
 
-#subsetting data
-subdata <- subset(data, subset=(data$Date=="1/2/2007" | data$Date=="2/2/2007")) 
-subdata$Date <- as.Date(subdata$Date, format="%d/%m/%Y")
-subdata$Time <- strptime(subdata$Time, format="%H:%M:S%")
-subdata[1:1440, "Time"] <- format(subdata[1:1440, "Time"], "2007-02-01 %H:%M:S%")
-subdata[1441:2880, "Time"] <- format(subdata[1441:2880, "Time"], "2007-02-01 %H:%M:S%")
-
-#Plotting and saving the data 
-dev.copy(png, file="plot2.png", height=480, width=480)
-plot(subdata$Time, as.numeric(as.character(subdata$Global_active_power)), type="l", xlab="", ylab="Global Active Power (In KW)")
+##Creating variables to construct the desired plot
+dateTime <- strptime(paste(subsetDataFile$Date, subsetDataFile$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+globalActivePower <- as.numeric(subsetDataFile$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(dateTime, globalActivePower, type='l', xlab="", ylab="Global Active Power (in Kw)")
 dev.off()
-
-
-
