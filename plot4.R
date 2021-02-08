8b9c48d995503c1bbb8cd6793771d4bf78c2c3a6
conwayza <77240614+conwayza@users.noreply.github.com>
2021-02-08 08:56:46 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,33 +1,25 @@
-#getting data
+##Collecting the information from the text file and creating a subset for it
 dataFile <- "./household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".") 
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subsetDataFile <- data[data$Date %in% c("1/2/2007", "2/2/2007") ,]
 
-#subsetting data
-subsetData <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))  
+##Creating variables to construct the desired plot
+dateTime <- strptime(paste(subsetDataFile$Date, subsetDataFile$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+globalActivePower <- as.numeric(subsetDataFile$Global_active_power)
+globalReactivePower <- as.numeric(subsetDataFile$Global_reactive_power)
+voltage <- as.numeric(subsetDataFile$Voltage)
+subMetering1 <- as.numeric(subsetDataFile$Sub_metering_1)
+subMetering2 <- as.numeric(subsetDataFile$Sub_metering_2)
+subMetering3 <- as.numeric(subsetDataFile$Sub_metering_3)
 
-dateTime <- strptime(paste(subsetData$Date, subsetData$Time), "%d/%m/%Y %H:%M:%S")
-dateTime <- as.POSIXct(dateTime)
-globalActivePower <- as.numeric(subsetData$Global_active_power)
-subMetering1 <- as.numeric(subsetData$Sub_metering_1)
-subMetering2 <- as.numeric(subsetData$Sub_metering_2)
-subMetering3 <- as.numeric(subsetData$Sub_metering_3)
-
-#Creating the png
-dev.copy(png, file="plot4.png", height=480, width=480)
-par(mfrow = c(2,2))
-attach(data)
-plot(dateTime, Voltage, type="l")
+#Using the data above to create the desired plot
+png("plot4.png", width=480, height=480)
+par(mfrow=c(2,2))
+plot(dateTime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+plot(dateTime, voltage, type="l", xlab="Datetime", ylab="Voltage")
 plot(dateTime, subMetering1,  type="l", ylab="Energy Submetering", xlab="")
 lines(dateTime, subMetering2,  type="l", col="Red")
 lines(dateTime, subMetering3,  type="l", col="Blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, bty="n", col=c("black", "red", "blue"))
-
-plot(dateTime, Global_reactive_power, type="l")
-
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"), bty="o") 
+plot(dateTime, globalReactivePower, type="l", xlab="Datetime", ylab="Global Reactive Power")
 dev.off()
-detach(data)
-
-#For whomever is reviewing, I could not get the last 3 graphs to work due to the following error: 
-#Error in plot.window(...) : need finite 'xlim' values
-#In addition: Warning messages:
-#The file's syntax made sense to me, however, I could not figure out how to get the 'xlim' value error to clear
